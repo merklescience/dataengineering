363100c8ef1f00d9978603e4260a921560d9ecbc
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-02-10 12:45:18 +0530

    docs: add docstrings for all modules

--- dataengineering/__init__.py
+++ dataengineering/__init__.py
@@ -1,1 +1,8 @@
+"""dataengineering python SDK
+
+This library contains all the code that we reuse here at Merkle Science Data Engineering.
+This includes, but is not limited to, utility functions and Apache Airflow operators.
+
+See the project's README for more information regarding the development and usecases.
+"""
 __version__ = "0.1.0"
---
+++ dataengineering/utils/__init__.py
@@ -0,0 +1,1 @@
+"""This module houses the utility functions."""
