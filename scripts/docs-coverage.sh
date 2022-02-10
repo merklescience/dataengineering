308360dece486ce7ca90cdf7a17bc14fc7e023da
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-02-10 14:29:00 +0530

    chore: use custom script to run interrogate action

---
+++ scripts/docs-coverage.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# This script only exists for the sake of the github action
+python -m poetry run interrogate -v --output docs-coverage.txt --fail-under 50 --ignore-init-method --ignore-init-module --ignore-magic --no-color --generate-badge . --badge-format svg
+if grep -q PASSED docs-coverage.txt; then
+    exit 0
+else:
+    cat docs-coverage.txt
+exit 1
