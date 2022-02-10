cd12b45fa11eeca1d0b46d4ba9e8570a817feb7f
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-02-10 14:45:28 +0530

    chore: fix interrogate script, end if statement

--- scripts/docs-coverage.sh
+++ scripts/docs-coverage.sh
@@ -3,6 +3,7 @@
 python -m poetry run interrogate -v --output docs-coverage.txt --fail-under 50 --ignore-init-method --ignore-init-module --ignore-magic --no-color --generate-badge . --badge-format svg
 if grep -q PASSED docs-coverage.txt; then
     exit 0
-else:
+else
     cat docs-coverage.txt
     exit 1
+fi
