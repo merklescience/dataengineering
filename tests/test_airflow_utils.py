a7a22d3f0cb71298015f49d324f6755c787078c0
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-07-29 17:03:21 +0530

    feat: add test for build_bigquery_destination
    * test that the behaviour of `build_bigquery_destination` is as expected
      for all scenarios
    * Add mock function for `airflow.models.Variable.get` for the above
      test.

--- tests/test_airflow_utils.py
+++ tests/test_airflow_utils.py
@@ -1,5 +1,9 @@
 """Tests the airflow utility functions"""
+import os
+from unittest.mock import Mock, patch
+
 import pytest
+from airflow.models import Variable, variable
 
 
 def test_import():
@@ -31,3 +35,37 @@ def test_apply_env_variables_on_blob():
     assert (
         result == expected_result
     ), "Templating function couldn't account for templates with `[[]]`."
+
+
+def variable_get(variable_name):
+    """Patched function that one can use instead of `airflow.models.Variable.get`"""
+    if variable_name == "BIGQUERY_DESTINATION_PROJECT":
+        return "test-project"
+    if variable_name == "BIGQUERY_DESTINATION_DATASET":
+        return "test-dataset"
+
+
+@pytest.mark.parametrize(
+    "server_env,dest_id",
+    [
+        ("local", "test-project.test-dataset.table-name"),
+        ("dev", "test-project.test-dataset.table-name"),
+        ("production", "test-project.prod-dataset.table-name"),
+    ],
+)
+def test_build_bigquery_destination_nonproduction(server_env, dest_id):
+    """Tests that you can build the exact table name given airflow variables for a non-prod environment"""
+    from dataengineering.airflow.bigquery.utils import build_bigquery_destination
+
+    os.environ["SERVER_ENV"] = server_env
+    mocked_variable_get = Mock()
+    mocked_variable_get.side_effect = variable_get
+    with patch("airflow.models.Variable.get", mocked_variable_get):
+        bigquery_desination = build_bigquery_destination("prod-dataset", "table-name")
+    assert (
+        bigquery_desination == dest_id
+    ), f"Unable to build `{server_env}` bigquery destination"
+
+
+def test_build_bigquery_destination_production():
+    """Tests that you can build the exact table name given airflow variables for a production server environment"""
