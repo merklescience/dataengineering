2d3ea1e5eabc3ec070cec61496f4f67e024b91e5
aniket818 <aniket818mazumdar@gmail.com>
2022-08-10 13:42:12 +0530

    fix: updating slack notfication util to only run on production

--- dataengineering/utils/notifications.py
+++ dataengineering/utils/notifications.py
@@ -1,7 +1,9 @@
 import json
-import os
 
 import requests
+from decouple import config
+
+from dataengineering.constants import ServerEnv
 
 
 def task_fail_slack_alert(context):
@@ -10,14 +12,16 @@ def task_fail_slack_alert(context):
     This function needs to be attached to all dags from which you
     monitor failures
     """
-    slack_webhook_url = os.getenv("SLACK_NOTIFICATION_URL")
+    slack_webhook_url = config("SLACK_NOTIFICATION_URL", cast=str)
+    server_env = config("SERVER_ENV", ServerEnv.LOCAL, cast=str)
 
-    slack_msg = f"""
-            :red_circle: Task Failed.
-            *Task*: {context.get('task_instance').task_id}
-            *Dag*: {context.get('task_instance').dag_id}
-            *Execution Time*: {context.get('execution_date')}
-            *Log Url*: {context.get('task_instance').log_url}
-            """
+    if server_env == ServerEnv.PRODUCTION:
+        slack_msg = f"""
+                :red_circle: Task Failed.
+                *Task*: {context.get('task_instance').task_id}
+                *Dag*: {context.get('task_instance').dag_id}
+                *Execution Time*: {context.get('execution_date')}
+                *Log Url*: {context.get('task_instance').log_url}
+                """
 
-    requests.post(slack_webhook_url, data=json.dumps({"text": slack_msg}))
+        requests.post(slack_webhook_url, data=json.dumps({"text": slack_msg}))
