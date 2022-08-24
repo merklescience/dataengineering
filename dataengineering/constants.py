62cd1d2c6afc74a7d30b461902101e8f6a73e549
AroraShreshth <shreshth@merklescience.com>
2022-08-24 14:58:20 +0530

    CoinPriceEnv Constants

--- dataengineering/constants.py
+++ dataengineering/constants.py
@@ -1,5 +1,12 @@
+from decouple import config
 class ServerEnv:
 
     LOCAL = "local"
     DEV = "dev"
     PRODUCTION = "production"
+
+class CoinPriceEnv:
+
+    PRICING_SERVICE_URL = config.get("PRICING_SERVICE_URL")
+    COINGECKO_URL = config.get("COINGECKO_URL")
+    PRICING_SERVICE_TOKEN = config.get("PRICING_SERVICE_TOKEN")
