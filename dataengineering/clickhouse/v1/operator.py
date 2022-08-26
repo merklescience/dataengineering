7a7cd77bc8806978c27da5de7c0b0b9437e54679
AroraShreshth <shreshth@merklescience.com>
2022-08-26 14:31:55 +0530

    fix : import dataengineering.utils.bitquery fix

--- dataengineering/clickhouse/v1/operator.py
+++ dataengineering/clickhouse/v1/operator.py
@@ -4,7 +4,7 @@ from typing import Optional, Dict, Any
 from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 from airflow.models import BaseOperator, Variable
 
-from utils.bitquery import get_synced_status
+from dataengineering.utils.bitquery import get_synced_status
 from dataengineering.coinprice.utils import get_latest_token_prices
 from dataengineering.clickhouse.v1.bash_hook import ClickHouseBashHook
 import jinja2
