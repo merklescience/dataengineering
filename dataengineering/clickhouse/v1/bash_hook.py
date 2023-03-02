825f911286ccf12775ce5aaccb64ddd022068c0a
Manthan Gupta <manthangupta109@gmail.com>
2023-03-02 20:24:29 +0530

    fix

--- dataengineering/clickhouse/v1/bash_hook.py
+++ dataengineering/clickhouse/v1/bash_hook.py
@@ -95,7 +95,7 @@ class ClickHouseBashHook(object):
                 try:
                     from airflow.models.variable import Variable
 
-                    conn = json.dumps(config(self.clickhouse_conn_id))
+                    conn = config(self.clickhouse_conn_id)
                     return self._check_json_values(
                         conn_details=conn, key_type="Variables"
                     )
